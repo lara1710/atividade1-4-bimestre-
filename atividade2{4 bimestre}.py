03972e90d61b05690eb28312f5f56a6bd2c3d6ce
lara1710 <laraleticia1203@gmail.com>
2024-10-16 13:40:59 -0300

    cl4rx

---
+++ atividade2{4 bimestre}.py
@@ -0,0 +1,8 @@
+# Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
+
+numero = int(input("digite seu numero: "))
+if numero  % 2 == 0:
+    print("par")
+else:
+    print("impar")
+
